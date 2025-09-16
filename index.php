850b15f9d33e52467fd9c6e8fe7fd5c7f55efa60
igorDeveloper <138522037+igor-gdf@users.noreply.github.com>
2025-09-16 19:39:15 +0000

    metodos

--- index.php
+++ index.php
@@ -5,59 +5,14 @@ require __DIR__ . "/src/modelo/filme.php";
 
 echo "Bem-vindo(a) ao screen match!\n";
 
-$nomeFilme = "Top Gun - Maverick";
+$filme = new Filme();
+$filme -> defineAnoLancamento(2021);
+$filme -> defineNome("Thor - Raghnarok");
+$filme -> defineGenero("ação");
+$filme -> avalia(10);
+$filme -> avalia(6);
+$filme -> avalia(7.8);
+$filme -> avalia(8.2);
 
-$anoLancamento = 2022;
-
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
-
-for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $argv[$contador];
-}
-
-if ($quantidadeDeNotas > 0) {
-    $notaFilme = array_sum($notas) / $quantidadeDeNotas;
-} else {
-    $notaFilme = 0; // Ou algum valor padrão, ou mensagem de erro
-    echo "Nenhuma nota foi informada. Não é possível calcular a média.\n";
-}
-
-$planoPrime = true;
-
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
-
-echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: $notaFilme\n";
-echo "Ano de lançamento: $anoLancamento\n";
-
-exibeMensagemLancamento($anoLancamento);
-
-$genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação",
-    "Thor: Ragnarok" => "super-herói",
-    "Se beber não case" => "comédia",
-    default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero\n";
-
-$filme = criarFilme(
-    nome: "Thor: ragnarok",
-    anoLancamento: 2021,
-    nota: 7.8,
-    genero: "super-heroi"
-);
-
-echo $filme->anoLancamento."\n";
-
-if (!empty($notas)) {
-    $menorNota = min($notas);
-    echo $menorNota . "\n";
-} else {
-    echo "Nenhuma nota informada para encontrar a menor nota.\n";
-}
-
-$filmeComoStringJason = json_encode($filme);
-
-file_get_contents(__DIR__ . "/filme.json", $filmeComoStringJason);
+echo $filme -> media()."\n";
+echo $filme -> anoLancamento()."\n";
--- src/modelo/filme.php
+++ src/modelo/filme.php
@@ -1,8 +1,40 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public float $nota;
+    private string $nome = "nome padrão";
+    private int $anoLancamento = 2024;
+    private string $genero = "ação";
+    private float $media = 10;
+    private array $notas = [];
+
+    public function avalia(float $nota): void {
+        $this->notas[] = $nota;
+    }
+    public function media(): float {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / count($this->notas);
+    }
+    public function nome():string {
+        return $this -> nome;
+    }
+    public function defineNome(string $nome): void {
+        $this -> nome = $nome;
+    }
+    public function anoLancamento():int {
+        return $this -> anoLancamento;
+    }
+    public function defineAnoLancamento(int $anoLancamento): void {
+        $this -> anoLancamento = $anoLancamento;
+    }
+    public function genero(): string
+    {
+        return $this->genero;
+    }
+
+    public function defineGenero(string $genero): void
+    {
+        $this-> genero = $genero;
+    }
 };
